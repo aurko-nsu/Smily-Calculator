a1a09b59c668b874c1627eeabe84550060cc0d86
aurko_office <mahmudur.rashid@northsouth.edu>
2022-07-05 18:26:26 +0600

    css and html form

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -5,23 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="{{ url('css/myCustom.css') }}">
+    <script src="https://unpkg.com/vue@3"></script>
     <title>Calculator</title>
 </head>
 <body>
-    <div class="main_body">
-        <form action="">
-            <input type="text" name="number1" placeholder="Number One">
-            <select name="operator" id="operator">
-                <option value="" selected></option>
-                <option value="+">&#128125;</option>
-                <option value="-">&#128128;</option>
-                <option value="*">&#128123;</option>
-                <option value="/">&#128561;</option>
-            </select>
-            <input type="text" name="number2" placeholder="Number Two">
-            <br>
-            <button type="submit">Calculate</button>
-        </form>
-    </div>
+<form action="">
+    <input type="text" name="number1" placeholder="Number One">
+    <select name="operator" id="operator">
+        <option value="" selected></option>
+        <option value="+">&#128125;</option>
+        <option value="-">&#128128;</option>
+        <option value="*">&#128123;</option>
+        <option value="/">&#128561;</option>
+    </select>
+    <input type="text" name="number2" placeholder="Number Two">
+    <br>
+    <button type="submit">Calculate</button>
+</form>
 </body>
 </html>
