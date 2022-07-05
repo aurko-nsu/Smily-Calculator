672a24bf1aa5bcf8f2a01b671166723b06553e54
aurko_office <mahmudur.rashid@northsouth.edu>
2022-07-05 14:12:55 +0600

    smiley added

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -13,10 +13,10 @@
             <input type="text" name="number1" placeholder="Number One">
             <select name="operator" id="operator">
                 <option value="" selected></option>
-                <option value="+">+</option>
-                <option value="-">-</option>
-                <option value="*">x</option>
-                <option value="/">/</option>
+                <option value="+">&#128125;</option>
+                <option value="-">&#128128;</option>
+                <option value="*">&#128123;</option>
+                <option value="/">&#128561;</option>
             </select>
             <input type="text" name="number2" placeholder="Number Two">
             <br>
